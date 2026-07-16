e97e74740730a3cbc29521d9ed921fd804fb2034
akalkannan299-byte <akalkannan299@gmail.com>
2026-07-16 21:57:34 +0530

    Create ScientificCalculator.cpp

---
+++ ScientificCalculator.cpp
@@ -0,0 +1,50 @@
+# Scientific Calculator using C++
+
+## Project Description
+A menu-driven scientific calculator developed using C++.
+
+## Features
+
+### Basic Operations
+- Addition
+- Subtraction
+- Multiplication
+- Division
+
+### Scientific Operations
+- Logarithm
+- Power
+- Square root
+- Factorial
+- nCr and nPr
+
+### Trigonometric Operations
+- sin
+- cos
+- tan
+- sec
+- cosec
+- cot
+
+### Matrix Operations
+- Addition
+- Subtraction
+- Multiplication
+- Transpose
+- Determinant
+
+### Complex Number Operations
+- Addition
+- Subtraction
+- Multiplication
+- Division
+- Conjugate
+- Polar conversion
+
+## Technologies Used
+- C++
+- OOP Concepts
+- Mathematical Functions
+
+## Author
+Akalya Kannan
